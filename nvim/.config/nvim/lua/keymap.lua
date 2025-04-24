8422540bf96b481e1433daa391c516404e1aea6e
Victor Melonio <victor@victorpfm.dev>
2025-04-23 21:37:42 -0300

    (nvim): add next/prev buffer keybindings

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -8,11 +8,16 @@ vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set("n", "[b", "<cmd>bp<CR>", { desc = "Go to previous [b]uffer"})
+vim.keymap.set("n", "]b", "<cmd>bn<CR>", { desc = "Go to next [b]uffer"})
 vim.keymap.set("n", "[t", "<cmd>tabprevious<CR>", { desc = "Go to previous [t]ab"})
 vim.keymap.set("n", "]t", "<cmd>tabnext<CR>", { desc = "Go to next [t]ab"})
 
